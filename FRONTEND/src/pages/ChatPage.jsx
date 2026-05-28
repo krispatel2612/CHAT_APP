7828164c0e6b2186f08a6b7b1e0a668abded051c
krispatel2612 <krispatel2605@gmail.com>
2026-05-28 20:23:18 +0530

    chat page layout setup

--- FRONTEND/src/pages/ChatPage.jsx
+++ FRONTEND/src/pages/ChatPage.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
-import { useAuthStore } from '../store/useAuthStore';
+import { useChatStore } from '../store/useChatStore';
+import BorderAnimatedContainer from '../components/BorderAnimatedContainer';
+import ActiveTabSwitch from '../components/ActiveTabSwitch';
+import ProfileHeader from '../components/ProfileHeader';
+import ContactList from '../components/ContactList';
+import ChatsList from '../components/ChatsList';
+import ChatContainer from '../components/ChatContainer';
+import NoConversationPlaceholder from '../components/NoConversationPlaceholder';
 
 function ChatPage() {
-  const {logout}=useAuthStore();
-  
+  const { activeTab ,selectedUser} = useChatStore();
+
   return (
-  <div className="z-10">
-    ChatPage
-    <button onClick={logout}>logout</button>
+    <div className="relative w-full max-w-6xl h-[800px]">
+      <BorderAnimatedContainer>
+        {/* left side */}
+        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+          <ProfileHeader />
+          <ActiveTabSwitch />
+          <div className="flex-1 overflow-y-auto p-4 space-y-2">
+            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+          </div>
+        </div>
+        {/* right side */}
+        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
+        {selectedUser ? <ChatContainer/> :<NoConversationPlaceholder/>}
+        </div>
+      </BorderAnimatedContainer>
+
     </div>
   );
 }
