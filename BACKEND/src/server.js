70f171e929d55c6a1ead0e3857ae967fc496d2e4
krispatel2612 <krispatel2605@gmail.com>
2026-05-28 13:46:32 +0530

    signup page ui added

--- BACKEND/src/server.js
+++ BACKEND/src/server.js
@@ -6,16 +6,17 @@ import messageRoutes from "./routes/message.route.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
-
+import cors from "cors";
 
 
 
 const app = express();
 const __dirname = path.resolve(); 
 
-const PORT = ENV.PORT;
+const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
